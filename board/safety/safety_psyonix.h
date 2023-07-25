53ece74e1b36b878be5cb50504c1b62cd2ea8be0
Jason Young <jyoung8607@gmail.com>
2023-07-25 12:41:19 -0400

    temp fwd hook

--- board/safety/safety_psyonix.h
+++ board/safety/safety_psyonix.h
@@ -1,7 +1,20 @@
+static int psyonix_fwd_hook(int bus_num, int addr) {
+  int bus_fwd = -1;
+  UNUSED(addr);
+
+  if (bus_num == 0) {
+    bus_fwd = 2;
+  }
+  if (bus_num == 2) {
+    bus_fwd = 0;
+  }
+
+  return bus_fwd;
+}
 const safety_hooks psyonix_hooks = {
   .init = nooutput_init,
   .rx = default_rx_hook,
   .tx = nooutput_tx_hook,
   .tx_lin = nooutput_tx_lin_hook,
-  .fwd = alloutput_fwd_hook,
+  .fwd = psyonix_fwd_hook,
 };
